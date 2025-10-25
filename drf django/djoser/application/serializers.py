53243ca539a3b9239d25b445aef729789c89e22a
Ammar <150323504+ammarproficient@users.noreply.github.com>
2025-10-25 22:28:05 +0500

    Delete drf django/djoser directory

--- drf django/djoser/application/serializers.py
+++
@@ -1,7 +0,0 @@
-from djoser.serializers import UserCreateSerializer
-from .models import User
-
-class UserSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
-        model = User
-        fields = ('id', 'email', 'name', 'password')
