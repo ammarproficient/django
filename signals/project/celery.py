4d8e9e677d8ca6469d710376e9775facd64f28b4
Ammar <150323504+ammarproficient@users.noreply.github.com>
2025-10-30 12:07:38 +0500

    Add instructions for running Celery worker
    Added usage instructions for running the Celery worker.

--- signals/project/celery.py
+++ signals/project/celery.py
@@ -6,3 +6,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 app = Celery('project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+# To run this file run this command
+# celery -A project worker -l info -P solo
